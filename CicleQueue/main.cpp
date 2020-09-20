a8d919dfee8009c0bd895388f31772585cc17ca2
Maxim <max77648@gmail.com>
2020-09-20 14:04:04 +0300

    Очередь из массива

--- CicleQueue/main.cpp
+++ CicleQueue/main.cpp
@@ -2,161 +2,196 @@
 
 using namespace std;
 
-class CicleQueue
+class Queue
 {
 	private:
-	struct Node
-	{
-		public:
-		string Value;
-		Node* Next;
-		public:
-		Node(): Next(NULL) {}
-		Node(const string& value): Value(value), Next(NULL) {}
-	};
-	
-	private:
-	int _size;
-	int _countElements;
+	int _maxSize;
 	int _currentIndex;
-	Node* _head;
-	Node* _tail;
-	bool _isOwerflow;
-	
+	int _countElements;
+	int _indexIfQueueIsFull;
+	string* _memory;
+
 	public:
-	CicleQueue(int size)
+	Queue(int maxSize)
 	{
-		_size = size;
+		_maxSize = maxSize;
 		_countElements = 0;
-		_currentIndex = -1;
-		_head = _tail = NULL;
-		_isOwerflow = false;
+		_currentIndex = 0;
+		_indexIfQueueIsFull = 0;
+		_memory = new string[_maxSize];
+	}
+	
+	Queue(const Queue& q)
+	{
+		_maxSize = q._maxSize;
+		_countElements = q._countElements;
+		_currentIndex = q._currentIndex;
+		_indexIfQueueIsFull = q._indexIfQueueIsFull;
+		_memory = q._memory;
 	}
 
-	private:
-
-	bool IsFull()
+	~Queue()
 	{
-		return _countElements == _size;
-	}
-
-	void Delete()
-	{
-		Node* newTail = _tail->Next;
-		_tail = new Node();
-		_tail = newTail;
-	}
-
-	void PushByNode(Node* headOrTail, const string& newElement)
-	{
-		Node* newNode = new Node(newElement);
-		if(_head != NULL)
-		{
-			headOrTail->Next = newNode;
-			headOrTail = newNode;
-		}
-		else
-		{
-			_head = _tail = newNode;
-		}
-		++_countElements;
-	}
-
-	void PushBack(const string& newElement)
-	{
-		PushByNode(_tail, newElement);
-	}
-
-	void PushFrot(const string& newElement)
-	{
-		PushByNode(_head, newElement);
-	}
-
-	void PushByIndex(int index)
-	{
-		Node* newElement = _head;
-		while(index-- > 0)
-		{
-			newElement = newElement->Next;
-		}
-
+		Clear();
 	}
 
 	public:
 
-	void Enqueue(const string& newElement)
+	void Enqueue(const string & element)
 	{
-		if(IsFull() != NULL)
+		if(IsFull() && _currentIndex == 0)
 		{
-
+			AddNewElement(++_indexIfQueueIsFull, element);
+		}
+		else if(IsFull())
+		{
+			_currentIndex = 0;
+			AddNewElement(_currentIndex, element);
 		}
 		else
 		{
-			cout << "Queue is full" << endl;
+			AddNewElement(_currentIndex, element);
+			++_countElements;
+			++_currentIndex;
 		}
-	}
-
-	int GetFrontSize()
-	{
-		return _head->Value.size();
+		
 	}
 
 	void Dequeue()
 	{
-		Node* oldElement = _head;
-		_head = _head->Next;
-		delete oldElement;
-		--_countElements;
-	}
-
-	void Clear()
-	{
-		while(_head)
+		if(IsEmpty() == false)
 		{
-			Dequeue();
+			DeleteFront();
+			_currentIndex;
 		}
 	}
 
 	void ShowInfo()
 	{
-		cout << "Size: " << _size << endl;
+		cout << "Size: " << _maxSize << endl;
 		cout << "Elements: " << _countElements << endl;
 		if(_countElements > 0)
 		{
-			Node* temp = _head;
-			while(temp->Next != NULL)
+			for(int i = 0; i < _countElements; i++)
 			{
-				cout << temp->Value << endl;
-				temp = temp->Next;
+				cout << _memory[i] << endl;
 			}
-			cout << temp->Value << endl;
+		}
+	}
+
+	int GetSizeFront()
+	{	
+		if(IsEmpty() == false)
+		{
+			return _memory[0].size();
+		}
+	}
+
+	int GetNumberOfElements()
+	{
+		return _countElements;
+	}
+
+	void Clear()
+	{
+		_maxSize = 0;
+		_countElements = 0;
+		_currentIndex = 0;
+		_indexIfQueueIsFull = 0;
+		delete _memory;
+	}
+	
+	string Peek()
+	{
+		if(IsEmpty() == false)
+		{
+			string temp = _memory[0];
+			DeleteFront();
+			return temp;
 		}
 	}
 	
-	/*void CopyFromFront(string *value)
-	{
-		value = &_head->Value;
-	}*/
-};
+	private:
 
+	bool IsEmpty()
+	{
+		return _countElements == 0;
+	}
+
+	bool IsFull()
+	{
+		return _countElements == _maxSize;
+	}
+
+	void AddNewElement(int index, string element)
+	{
+		_memory[index] = element;
+	}
+
+	void DeleteFront()
+	{
+		for(int i = 1; i < _countElements; i++)
+		{
+			_memory[i - 1] = _memory[i];
+		}
+		--_countElements;
+	}
+};
 
 int main()
 {
-	CicleQueue q(3);
-	q.Enqueue("1123123");
-	q.Enqueue("22");
-	q.Enqueue("333");
-	//q.ShowInfo();
-	
-	//cout << q.GetFrontSize() << endl;
+	Queue q1(3);
 
-	/*string* value;
-	q.CopyFromFront(value);
-	cout << value << endl;*/
+	q1.Enqueue("1");
+	q1.Enqueue("22");
 	
-	//q.Dequeue();
+	cout << q1.GetSizeFront() << endl;
 
-	q.Clear();
+	Queue q2(q1);
+	
+	q1.Dequeue();
+
+	cout << q1.GetNumberOfElements() << endl;
+
+	cout << q1.Peek() << endl;
+
+	q1.Clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//CicleQueue q(3);
+	//q.Enqueue("1123123");
+	//q.Enqueue("22");
+	//q.Enqueue("333");
+	////q.ShowInfo();
+	//
+	////cout << q.GetFrontSize() << endl;
+
+	///*string* value;
+	//q.CopyFromFront(value);
+	//cout << value << endl;*/
+	//
+	////q.Dequeue();
+
+	//q.Clear();
 
 	//cout << << endl;
 	return 0;
